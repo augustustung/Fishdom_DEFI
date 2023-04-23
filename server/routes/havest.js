ccbe73a26d604e8f5a666bddf65b298671c56cbc
augustusflynn <huytung139@gmail.com>
2023-04-23 13:42:24 +0700

    initializing v1

--- server/routes/havest.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const auth = require("../middlewares/auth");
-const manager = require('../HavestStaking/manager');
-
-router.post("/get", auth, manager.getList);
-
-module.exports = router;
